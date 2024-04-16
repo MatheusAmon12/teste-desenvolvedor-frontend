c4ce03e5451f5f2d9f338478878ef67f9f2a0c4a
Matheus Amon <amonmatheus757@gmail.com>
2024-04-16 15:38:57 -0300

    feat(search): adiciona componente de pesquisa

---
+++ bulario-eletronico/src/components/Search.js
@@ -0,0 +1,14 @@
+import { TextField } from "@mui/material"
+
+
+const Search = () => {
+    return(
+        <TextField 
+            size="small" 
+            fullWidth 
+            variant="outlined" 
+        />
+    )
+}
+
+export default Search
